bcab97f1c0b49222cf418c16fae1d2433dac149d
ABHISHEK KUMAR <100033618+abhishek110803@users.noreply.github.com>
2024-03-03 19:17:46 +0530

    Updated Controllers

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -1,20 +1,7 @@
 import nodemailer from "nodemailer";
 
 // async..await is not allowed in global scope, must use a wrapper
-const sendEmail = async function (email, subject, message, cc) {
-  // create reusable transporter object using the default SMTP transport
-  // let transporter = nodemailer.createTransport({
-  //   host: process.env.SMTP_HOST,
-  //   port: process.env.SMTP_PORT,
-  //   secure: false, // true for 465, false for other ports
-  //   auth: {
-  //     user: process.env.SMTP_USERNAME,
-  //     pass: process.env.SMTP_PASSWORD,
-  //   },
-  // });
-
-  // http://localhost:3000/reset-password/7ba15d7eeca68dc46a29b6d050a3200fd31f0cec
-
+export const sendEmail = async function (email, subject, message) {
 
   const transportOptions = {
     host: "smtp.gmail.com",
@@ -41,4 +28,25 @@ const sendEmail = async function (email, subject, message, cc) {
   });
 };
 
-export default sendEmail;
+
+export const sendEmailContact = async function (email, subject, message) {
+  const transportOptions = {
+    host: "smtp.gmail.com",
+    port: 465,
+    secure: true,
+    auth: {
+      user: process.env.SENDER_EMAIL_CONTACT,
+      pass: process.env.SENDER_EMAIL_PASSWORD_CONTACT,
+    }
+  }
+
+  const transporter = nodemailer.createTransport(transportOptions);
+
+  await transporter.sendMail({
+    from: 'Utkansh support team', // sender address
+    to: email, // user email,
+    subject: subject, // Subject line
+    html: message, // html body
+  });
+};
+// export default sendEmail;
